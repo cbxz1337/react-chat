f1952a21c55ea34b5f74a635d450ca6ce4e4119f
Konstantin <rutbergh@inbox.ru>
2021-01-20 01:14:48 +0400

    Added autoscrolling

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,44 +1,55 @@
-import React,{useEffect, useState} from "react"
+import React,{useEffect, useState, useRef} from "react"
 import { socket } from "../../utils/socket"
 import "./style.css"
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 
 const Chat = ()=>{
 
+
     const [msg, setMsg] = useState("")
     const [chatdata, setChatData] = useState([])
 
+
+
     useEffect(()=>{
-        socket.on('data_chat_back_one', (data)=>{
+         socket.on('data_chat_back_one', (data)=>{
             setChatData(data)
         } ) }, [])
 
     function handleChange(event){
         setMsg(event.target.value)
     }
+    function handleKeyDown(event){
+        if(event.key == 'Enter'){
+            handleClick()
+        }
+    }
     const handleClick = ()=>{
-       socket.emit("msg_from_front", {
-           name: isMyself,
-           message: msg
-       })
-       setMsg("")
-       console.log(msg);
+            if(msg){
+            socket.emit("msg_from_front", {
+                name: isMyself,
+                message: msg
+            })
+            setMsg("")
+            console.log(msg);
+        }
     }
     const isMyself = localStorage.getItem("name")
 
 
     
     return <div className = "Chat">
-        <div className = "chatWindow">
+        <ScrollToBottom debug = {false} className = "chatWindow">
             {chatdata.map(({name, message}, index)=>{
                 return <div className ={(name===isMyself)? "chatItem myMsg":"chatItem"} key = {index}>
                     <span>{(name===isMyself)?"Вы":name}</span>
                     <p>{message}</p>
                 </div>
             })}
-        </div>
+        </ScrollToBottom>
         <div className = "msgBar">
-            <input className = "msgInput" placeholder = "Ваше сообщение ..." value = {msg} onChange = {handleChange}/>
+            <input className = "msgInput" placeholder = "Ваше сообщение ..." value = {msg} onChange = {handleChange} onKeyDown = {handleKeyDown} />
             <img src="https://via.placeholder.com/60" className = "msgButton" onClick = {handleClick}>
 
             </img>
