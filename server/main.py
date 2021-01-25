0a66be5fdb906da4f6b97c17e9d56e9470015d04
Konstantin <rutbergh@inbox.ru>
2021-01-25 19:25:02 +0400

    поправил окно с активными юзерами

--- server/main.py
+++ server/main.py
@@ -1,10 +1,10 @@
 import eventlet
 import socketio
 import json
+import os
 
 PORT = 2070
 HOST = 'localhost'
-
 
 sio = socketio.Server(cors_allowed_origins=[])
 app = socketio.WSGIApp(sio)
@@ -17,7 +17,6 @@ chatUsers = []
 @sio.on('msg_from_front')
 def on_message(sid, data):
     chatData.append(data)
-    a = list({v['name']: v for v in chatData}.values())
     sio.emit('data_chat_back_one', data)
     print(data)
     print(chatData)
@@ -25,17 +24,17 @@ def on_message(sid, data):
         json.dump(chatData, f, indent=5)
 
 
-def checkUser(data):
+def check_user(data):
     for dict in chatUsers:
         if list(dict.values()).__contains__(data):
             return True
 
 
-
 @sio.on('new_user_connected')
 def new_user_connected(sid, data):
-    if checkUser(data):
+    if check_user(data):
         sio.emit("check_username", True)
+        sio.emit('online_users', chatUsers)
         print("asd")
     else:
         chatUsers.append({'id': sid, 'name': data})
@@ -57,22 +56,14 @@ def disconnect(sid):
 
 if __name__ == '__main__':
     try:
-        with open('data.json', 'r') as f:
-            a = json.load(f)
-        chatData = a
-        eventlet.wsgi.server(eventlet.listen(('localhost', 2070)), app)
-        print("Server started")
-    except(Exception):
-            print("Server error")
-
-
-
-
-
-
-
-
-
-
-
-
+        if os.stat("data.json").st_size == 0:
+            eventlet.wsgi.server(eventlet.listen(('localhost', 2070)), app)
+            print("Server started")
+        else:
+            with open('data.json', 'w+') as f:
+                a = json.load(f)
+            chatData = a
+            eventlet.wsgi.server(eventlet.listen(('localhost', 2070)), app)
+            print("Server started")
+    except Exception:
+        print("Server error")
